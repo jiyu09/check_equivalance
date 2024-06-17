1b0595d014062e4437e7978809776dfbfb12aa70
jiyu09 <152246059+jiyu09@users.noreply.github.com>
2024-06-17 13:47:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-A = {1, 2, 3, 4}
-R = {(1, 1), (1, 3), (2, 2), (3, 3),(3, 1), (3, 4), (4, 4), (4, 3)}
+# A = {1, 2, 3, 4}
+# R = {(1, 1), (1, 3), (2, 2), (3, 3),(3, 1), (3, 4), (4, 4), (4, 3)}
 
-def cheak_reflexive(R) :
+def cheak_reflexive(R, A) :
   reflex = set([(x, x) for x in list(A)])
   return reflex.issubset(R)
 
@@ -19,7 +19,7 @@ def cheak_transitive(R) :
   transitive = set(transitive_list)
   return transitive.issubset(R)
 
-def check_equivalance(R) :
-  return cheak_reflexive(R) and cheak_symmetric(R) and cheak_transitive(R)
+def check_equivalance(R, A) :
+  return cheak_reflexive(R, A) and cheak_symmetric(R) and cheak_transitive(R)
 
-check_equivalance(R)
+check_equivalance(R, A)
